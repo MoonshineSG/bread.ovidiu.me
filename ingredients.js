8ef813afacea469e88732e7e71dceea66119267b
ǝuıɥsuooɯ <687747+MoonshineSG@users.noreply.github.com>
2024-10-29 09:59:50 +0800

    potato

--- ingredients.js
+++ ingredients.js
@@ -27,6 +27,7 @@ var other_ingredients = [
 	{ 'name': 'cardamon', 'hydration': 0, 'price': 0  },
 	{ 'name': 'cayenne_pepper', 'hydration': 0, 'price': 0  },
 	{ 'name': 'stout', 'hydration': 100, 'price': 4  },
+	{ 'name': 'potato', 'hydration': 75, 'price': 1.5  }
 
 ];
 
